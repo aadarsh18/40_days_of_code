c92755c9b8eb3f12ac2c501c3c4b54c67a59e17a
aadarsh18 <49119673+aadarsh18@users.noreply.github.com>
2019-11-13 12:58:37 +0530

    Update Dijkstra_algo.cpp

--- Dijkstra_algo.cpp
+++ Dijkstra_algo.cpp
@@ -16,96 +16,82 @@ using namespace std;
 #define ll long long int
 #define pb push_back
 #define pf push_front
+#define pii pair<ll,ll>
+#define mp make_pair 
 #define M 1000000007
 #define fastio ios_base::sync_with_stdio(false);
 
-struct data
-{
-    ll nod;
-    ll val;
-};
-
-// comperator function for priority queue 
 struct comp
 {
-    bool operator()(data const& s1, data const& s2)
+    bool operator()(const pair<int , int> &a , const pair<int , int> &b)
     {
-        return s1.val > s2.val;
+        return a.first > b.first;
     }
 };
 
-int main()
-{
-    fastio;
-
+ int main()
+ {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    
     ll n,m;
     cin>>n>>m;
-
+    vector<ll>G[n+2];
+    vector<ll>cost[n+2];
     ll i,x,y,w,mx=0;
-    vector<data>v[n+1];
-    data z1,z2;
-    
     for(i=0; i<m; i++)
     {
-    	cin>>x>>y>>w;
-        mx=mx+w;
-
-        z1.nod=y; z1.val=w;
-    	v[x].pb(z1);
-        z2.nod=x; z2.val=w;
-        v[y].pb(z2);
+        cin>>x>>y>>w;
+        
+        G[x].pb(y);
+        cost[x].pb(w);
+        G[y].pb(x);
+        cost[y].pb(w);
     }
-    mx=mx+100;
-
-    ll vis[n+1],dist[n+1];
+ /*   for(i=1; i<=n; i++)
+    {
+    	cout<<i<<" ";
+    	for(int j=0; j<G[i].size(); j++)
+    	{
+    		cout<<"("<<G[i][j]<<", "<<cost[i][j]<<")   ";
+    	}
+    	cout<<endl;
+    } cout<<endl; */
+	 
+    ll dist[n+2];
     for(i=1; i<=n; i++)
     {
-        vis[i]=0;
-        dist[i]=mx;
+        dist[i]=1000000000;
     }
+    priority_queue<pii, vector<pii>, comp>pq;
 
-    // source node 
-    ll s;
-    cin>>s;
-    
-    // define priority queue of structure 
-    priority_queue<data, vector<data>, comp>pq;
-    
-    z1.nod=s; z1.val=0;
-    pq.push(z1);
-    
-    vis[s]=1;
-    dist[s]=0;
+    ll source;
+    cin>>source;
 
+    dist[1]=0;
+    pq.push(mp(0, 1));
+    
     while(!pq.empty())
     {
-    	z1=pq.top();
-        ll p=z1.nod;   //cout<<p<<" ";
-        vis[p]=1;
+        ll node=pq.top().second;
         pq.pop();
         
-        // traverse in the adjacency list of p 
-        for(i=0; i<v[p].size(); i++)
+        for(i=0; i<G[node].size(); i++)
         {
-            if(dist[v[p][i].nod] > dist[p]+v[p][i].val )
+            ll it=G[node][i];
+            if(dist[it]>dist[node]+cost[node][i])
             {
-                dist[v[p][i].nod]=dist[p]+v[p][i].val;
-            }
-            
-            // if not visited push it in to priority_queue 
-            if(vis[v[p][i].nod]==0)
-            {
-                z2.nod=v[p][i].nod; z2.val=v[p][i].val;
-                pq.push(z2);
+               dist[it]=dist[node]+cost[node][i];
+               pq.push(mp(dist[it], it));
             }
         }
     }
-    //cout<<endl;
-    // Shortest distance from source to all other nodes  
+    
+    // distance from source to other
     for(i=1; i<=n; i++)
     {
         cout<<dist[i]<<" ";
     }
+     return 0;
+ }
 
-    return 0;
-} 
