a6c478edfdb3ded0c55f834beee3a91af09abcfd
aadarsh18 <49119673+aadarsh18@users.noreply.github.com>
2019-11-14 16:23:43 +0530

    Create Number_Of_Connected_Components_In_UD_Graph.cpp

---
+++ Number_Of_Connected_Components_In_UD_Graph.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<bitset>
+#include<deque>
+#include<iterator>
+#include<list>
+#include<map>
+#include<queue>
+#include<set>
+#include<stack>
+#include<unordered_map>
+#include<unordered_set>
+#include<vector>
+using namespace std;
+#define ll long long int
+#define pb push_back
+#define pf push_front
+#define M 1000000007
+#define fastio ios_base::sync_with_stdio(false);
+
+ll root(ll A[],ll i)
+{
+    while(A[i]!=i)
+    {
+        A[i]=A[A[i]];
+        i=A[i];
+    }
+    return i;
+}
+void unionn(ll A[],ll size[],ll a,ll b)
+{
+    ll root_a;
+    ll root_b;
+    root_a=root(A,a);
+    root_b=root(A,b);
+    if(size[root_a]<size[root_b])
+    {
+        A[root_a]=A[root_b];
+        size[b]+=size[a];
+    }
+    else
+    {
+        A[root_b]=A[root_a];
+        size[a]+=size[b];
+    }
+}
+bool find(ll A[],ll a,ll b)
+{
+    if(root(A,a)==root(A,b))
+    return true;
+    else
+    return false;
+}
+int main()
+{
+ 
+  ll n,m;
+  cin>>n>>m;
+  ll size[n+1],arr[n+1];
+  ll i,x,y;
+  for(i=1;i<=n;i++)
+  {
+      arr[i]=i;
+      size[i]=1;
+  }
+  for(i=1;i<=m;i++)
+  {
+      cin>>x>>y;
+      unionn(arr,size,x,y);
+  }
+  ll cnt=0;
+  for(i=1;i<=n;i++)
+  {
+      if(arr[i]==i)
+      cnt++;
+  }
+  cout<<cnt;
+return 0;
+}
